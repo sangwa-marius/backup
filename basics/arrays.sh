1adb86848c4ec7c0c27056b64183b066b6df2387
Marius <mariussangwa@gmail.com>
2026-04-03 01:37:24 +0300

    now I can loop through an array

--- basics/arrays.sh
+++ basics/arrays.sh
@@ -1,24 +1,31 @@
 
 
-arr=("apple" "banana" "cherry")
-echo ${arr[0]}
-arr[0]="grape"
-arr+=("orange")
-unset arr[1]
-echo ${arr[0]}
-echo ${arr[1]}
-echo ${arr[2]}
-echo ${arr[3]}
-echo ${arr[1]}
+# arr=("apple" "banana" "cherry")
+# echo ${arr[0]}
+# arr[0]="grape"
+# arr+=("orange")
+# unset arr[1]
+# echo ${arr[0]}
+# echo ${arr[1]}
+# echo ${arr[2]}
+# echo ${arr[3]}
+# echo ${arr[1]}
 
-# all elements
-echo " All elements: ${arr[@]}"
-# all elements as a single string
-echo " All elements as a single string: ${arr[*]}"
+# # all elements
+# echo " All elements: ${arr[@]}"
+# # all elements as a single string
+# echo " All elements as a single string: ${arr[*]}"
 
-#array length
-echo " Array length: ${#arr[@]}"
+# #array length
+# echo " Array length: ${#arr[@]}"
 
-#length of a specific element
-echo " Length of element at index 0: ${#arr[0]}"
+# #length of a specific element
+# echo " Length of element at index 0: ${#arr[0]}"
+
+arr2=(1 2 3 4 5)
+echo ${arr2[@]}
+
+for i in "${!arr2[@]}"; do
+    echo "Index: $i, Value: ${arr2[i]}"
+done
 
