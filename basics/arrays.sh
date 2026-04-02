f67794e90f1513da6e1fe43a2d89ecbf6c507983
Marius <mariussangwa@gmail.com>
2026-04-03 01:04:30 +0300

    added a basics folder

---
+++ basics/arrays.sh
@@ -0,0 +1,24 @@
+
+
+arr=("apple" "banana" "cherry")
+echo ${arr[0]}
+arr[0]="grape"
+arr+=("orange")
+unset arr[1]
+echo ${arr[0]}
+echo ${arr[1]}
+echo ${arr[2]}
+echo ${arr[3]}
+echo ${arr[1]}
+
+# all elements
+echo " All elements: ${arr[@]}"
+# all elements as a single string
+echo " All elements as a single string: ${arr[*]}"
+
+#array length
+echo " Array length: ${#arr[@]}"
+
+#length of a specific element
+echo " Length of element at index 0: ${#arr[0]}"
+
