5a3393e043bce0235d590b1f51c384818e5b6441
Marius <mariussangwa@gmail.com>
2026-05-03 10:30:11 +0200

    added other loops in array.sh

--- basics/arrays.sh
+++ basics/arrays.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-names=("Marius" "Sangwa" "Forever" "Rocky" "Marius" "Sangwa" "Forever" "Rocky")
+names=("Marius" "Sangwa" "Forever" "Rocky" "Marius" "Sangwa" "Forever" "Rocky");
 old_ifs=$IFS
 IFS=" "
 echo "names: ${names[@]}"
-echo "names in string form: ${names[*]}"
+echo "names
 echo "indices of names : ${!names[*]}"
 
 for i in "${names[@]}"; do
