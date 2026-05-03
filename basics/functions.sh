f26be40498bd7282f804b2f2f456894ccd25a248
Marius <mariussangwa@gmail.com>
2026-05-03 11:59:18 +0200

    added other functions

--- basics/functions.sh
+++ basics/functions.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
 
+# Defining a function using function keyword
 function greet() {
     echo "Hello, $1! you are $2 years old"
 }
 greet "Marius" "12"
+
+# Standard function definition
+my_function(){
+    echo "This i s a simple function"
+}
+my_function
+
+add(){
+   local sum=$(( $1 + $2 ))
+    echo $sum
+}
+result=$(add 5 10)
+echo "The sum is: $result"
+
+add_any(){
+    local sum=0
+    for num in "$@"; do 
+        sum=$((sum + num))
+    done
+    echo $sum
+}
+
+add_any 1 2 3 4 5 6
+
