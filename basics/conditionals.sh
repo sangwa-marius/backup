07888f9fabccad735941bd6f9f14c02eb26f9f99
Marius <mariussangwa@gmail.com>
2026-05-03 20:25:30 +0200

    formatted the conditional.sh using bash beauty

--- basics/conditionals.sh
+++ basics/conditionals.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 read -p "Enter a number: " num
-sleep 2;
+
 if [[ $num -gt 0 ]]; then
     echo "The number is positive"
-elif [[ $num -lt 0 ]]; then
+    elif [[ $num -lt 0 ]]; then
     echo "The number is negative"
 else
     echo "The number is zero"
