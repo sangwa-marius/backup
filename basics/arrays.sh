0b85528147420de2afc8ec4887e2cf53cf505828
Marius <mariussangwa@gmail.com>
2026-05-03 20:14:51 +0200

    fixed the bug in arrays.sh

--- basics/arrays.sh
+++ basics/arrays.sh
@@ -4,7 +4,6 @@ names=("Marius" "Sangwa" "Forever" "Rocky" "Marius" "Sangwa" "Forever" "Rocky");
 old_ifs=$IFS
 IFS=" "
 echo "names: ${names[@]}"
-echo "names
 echo "indices of names : ${!names[*]}"
 
 for i in "${names[@]}"; do
