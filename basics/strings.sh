de54bd18d5820de43bc9a58672c48ce541c6105e
Marius <mariussangwa@gmail.com>
2026-05-05 15:08:27 +0200

    added a file to study about bash strings

---
+++ basics/strings.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+RED="\033[0;31m"
+NC="\033[0m"
+
+string="Hello world"
+string2="Bash scripting is fun"
+result="$string $string2 is a powerful tool for automation and scripting tasks."
+
+echo $result
+
+printf "${RED}%i\n${NC}" "${#string}" #printing the length of a string in a red color
+
+echo "${string/w/bash}" # replacing the first occurrence of "w" with "bash"
+
+echo "${string//o/a}" # replacing all occurrences of "o" with "a"
+
+echo "${string^^}" # converting to uppercase
+
+echo "${string:0:4}" # extracting a substring from index 0 with length 4
+
+echo "${string%ld}" # removing the shortest match of "ld" from the end
+
+
+
