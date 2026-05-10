3f8a13f3c5b3015a3e8b998c7ecbe2a98793c83e
sangwa-marius <mariussangwa@gmail.com>
2026-05-10 23:55:55 +0200

    advanced write.sh

--- basics/Fileoperations/write.sh
+++ basics/Fileoperations/write.sh
@@ -18,17 +18,20 @@ if [[ $answer == "y" ]]; then
     sleep 3
     if [[ -f data.txt ]]; then
         echo "Contents of data.txt:"
-        cat data.txt
+        while IFS= read -r line;do
+		echo "$line"
+		sleep 1
+	done<data.txt
     else
         echo "data.txt does not exist."
     fi
 else
     echo "Okay, let's write some data to data.txt"
 fi
-read -p "Enter your name: " name
-read -p "Enter your age: " age
-read -p "Enter your city: " city
-read -p "Enter your country: " country
+read -p "Enter the name: " name
+read -p "Enter the age: " age
+read -p "Enter the city: " city
+read -p "Enter the country: " country
 
 echo "------------------------------------------" >> data.txt
 echo "Date: [$(date '+%Y-%m-%d %H:%M:%S')]" >> data.txt
