8f67ef5d90d0d33933dd41fb17c7efc99ee10193
Marius <mariussangwa@gmail.com>
2026-05-11 20:31:24 +0200

    added a file to demonstrate declarations and understanding types in bash

---
+++ basics/declarations.sh
@@ -0,0 +1,16 @@
+name="Sangwa Marius"
+declare -p name
+
+declare -i age=25
+declare -p age
+
+declare -a cities=("New York" "Paris" "Tokyo")
+declare -p cities
+
+declare -A user_info=(
+    ["name"]="Sangwa Marius"
+    ["age"]=25
+    ["city"]="New York"
+)
+
+declare -p user_info
