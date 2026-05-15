5946d607f04fe406b20f6be801bb9536840c4d43
Marius <mariussangwa@gmail.com>
2026-05-15 13:26:59 +0200

    used the color script in the conditionals

--- basics/conditionals.sh
+++ basics/conditionals.sh
@@ -1,10 +1,6 @@
-#!/usr/bin env bash
+#!/usr/bin/env bash
 
-RED="\033[0;31m"
-GREEN="\033[0;32m"
-BLUE="\033[0;34m"
-YELLOW="\033[1;33m"
-TRANSPARENT="\033[0m"
+. ../colors.sh
 
 while true; do
     read -p "Hello , I 'm Your age checker. Who are you? " name
