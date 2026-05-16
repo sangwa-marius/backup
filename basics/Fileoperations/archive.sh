3f0fd85df23531f7da33913b4b1034c5e62e897e
Marius <mariussangwa@gmail.com>
2026-05-16 21:23:21 +0200

    added a script to archive files

---
+++ basics/Fileoperations/archive.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+HOME="$HOME"
+
+read -p "Enter the name of the archive: " archive
+if [[ -z $archive ]]; then
+    echo "archive name should be provided! Exiting..."
+    sleep 2
+    exit 1
+fi
+
+if [[ ${#archive} -gt 10 ]]; then
+echo "archive name should be atmost 10 characters long"
+exit 1
+fi
+
+read -p "Enter the path of the files to archive: " -a files
+if [[ -z $files ]]; then
+echo "Files to archive are needed"
+exit 1
+fi
+
+archive_dir="$HOME/archives"
+
+if [[ ! -d $archive_dir ]]; then
+echo "Building archive directory..."
+sleep 2
+mkdir $archive_dir
+fi
+
+echo -e "Archiving the files..."
+tar -cvf "$archive.tar" "${files[@]}"
+mv "$archive.tar" $archive_dir
+
+echo -e "Files archived to path $archive_dir"
+
+
+
+
+
+
