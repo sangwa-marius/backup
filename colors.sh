3a7d8b515383d0c97fcd1be8eb95a820cd438013
Marius <mariussangwa@gmail.com>
2026-05-19 22:44:50 +0200

    RED_COLOR first

--- colors.sh
+++ colors.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
+RED="\033[0;31m"
 GREEN="\033[0;32m"
 BOLD_GREEN="\033[1;32m"
 YELLOW="\033[1;33m"
 BLUE="\033[0;34m"
-RED="\033[0;31m"
 PURPLE="\033[0;35m"
 BOLD_PURPLE="\033[1;35m"
 NC="\033[0m"
