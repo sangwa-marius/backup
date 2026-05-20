911a5eb5784d5ca80467855a9baf9e6015595a11
Marius <mariussangwa@gmail.com>
2026-05-20 15:00:42 +0200

    used N C instead of TRANSPARENT, because in colors script theres no TRASPARENT option

--- basics/conditionals.sh
+++ basics/conditionals.sh
@@ -7,14 +7,14 @@ while true; do
     read -p "Hello $name, How old are you?  "  age
     if [[ $age -lt 18 ]]; then
         remaining=$((18-age))
-        printf "${BLUE}Ooh! sorry %s you will be allowed to vote in next %i years\n${TRANSPARENT}" "$name" "$remaining"
+        printf "${BLUE}Ooh! sorry %s you will be allowed to vote in next %i years\n${NC}" "$name" "$remaining"
     else
-        printf "${GREEN}Congz %s, you can now vote\n${TRANSPARENT}" "$name"
+        printf "${GREEN}Congz %s, you can now vote\n${NC}" "$name"
     fi
     
     read -p "Do you wanna check for more people?[Y/N] " choice
     if [[  $choice != "Y" && $choice != "Yes" && $choice != "yes"  && $choice != "y" ]]; then
-        printf "${YELLOW}Thanks for using the age checker, see you later!${TRANSPARENT}\n"
+        printf "${YELLOW}Thanks for using the age checker, see you later!${NC}\n"
         break
     fi
     
