fac8d5e29ad1f716ced6c2e2d519e6c3a4ab263c
Marius <mariussangwa@gmail.com>
2026-05-22 01:09:59 +0200

    modified colors.sh to source the .colors.sh file in the home

--- colors.sh
+++ colors.sh
@@ -1,13 +1,3 @@
 #!/usr/bin/env bash
 
-RED="\033[0;31m"
-BOLD_RED="\033[1;31m"
-GREEN="\033[0;32m"
-BOLD_GREEN="\033[1;32m"
-YELLOW="\033[0;33m"
-BOLD_YELLOW="\033[1;33m"
-BLUE="\033[0;34m"
-BOLD_BLUE="\033[1;34m"
-PURPLE="\033[0;35m"
-BOLD_PURPLE="\033[1;35m"
-NC="\033[0m"
+source ~/.colors.sh
