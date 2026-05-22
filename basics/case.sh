6e4b221eaf38f3162658e070bfd1c188dbfc6e16
Marius <mariussangwa@gmail.com>
2026-05-22 21:19:37 +0200

    added grade indicator using case statements

--- basics/case.sh
+++ basics/case.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+source ~/.colors.sh
 
 read -p "Enter your age: " age
 
@@ -13,6 +14,31 @@ echo "You are a teen"
 
 *)
 echo "You are an adult"
+;;
+
+esac
+
+read -p "Enter your  marks: " marks
+
+case "$marks" in
+[0-5][0-9])
+echo "Grade F (00-59)"
+;;
+
+[6-7][0-9])
+echo "Grade C (60-79)"
+;;
+
+[8][0-9])
+echo "Grade B (80-89)"
+;;
+
+[9][0-9] | "100")
+echo "Grade A (90-100)"
+;;
+
+*)
+echo -e "${BOLD_RED}Marks out of bounds. Enter valid marks (00-100)${NC}"
 ;;
 
 esac
