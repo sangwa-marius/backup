f438538b70b2df90f23fdc032091c63f929c8bcd
Marius <mariussangwa@gmail.com>
2026-05-23 19:10:28 +0200

    modified the arrays scripts

--- basics/arrays.sh
+++ basics/arrays.sh
@@ -10,3 +10,6 @@ sorted_array=($(printf '%s \n' "${array[@]}" | sort))
 filtered_names=($(printf '%s \n' "${names[@]}" | grep "s"))
 echo "${sorted_array[@]}"
 echo "${filtered_names[@]}"
+unshift_names=("Okay" "${names[@]}");
+echo "${unshift_names[0]}"
+echo "$unset_names"
