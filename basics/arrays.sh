914bb584d64d5f3ac35baeb00d646609eb4bb7f8
Marius <mariussangwa@gmail.com>
2026-05-23 19:17:32 +0200

    added other array methods algorithms

--- basics/arrays.sh
+++ basics/arrays.sh
@@ -1,15 +1,28 @@
 #!/usr/bin/env bash
 
 array=(3 1 2 5)
+
 names=("Marius" "sangwa" "Ishimwe" "Rocky")
-IFS=","
+
+images=("photo.jpg" "marius.png" "coding.png")
+
+#replace
+replace_images=("${images[@]/.jpg/.png}")
+echo "${replace_images[0]}"
 
 echo "${array[*]}"
-echo "$IFS"
+
+#sort
 sorted_array=($(printf '%s \n' "${array[@]}" | sort))
+
+#fileter
 filtered_names=($(printf '%s \n' "${names[@]}" | grep "s"))
+
 echo "${sorted_array[@]}"
+
 echo "${filtered_names[@]}"
+
+#unshift
 unshift_names=("Okay" "${names[@]}");
+
 echo "${unshift_names[0]}"
-echo "$unset_names"
