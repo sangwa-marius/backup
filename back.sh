e74d90a08d73a7416e4499a40d1eaf9718569a70
Marius <mariussangwa@gmail.com>
2026-06-03 01:36:08 +0200

    formatted the back.sh script

--- back.sh
+++ back.sh
@@ -10,21 +10,21 @@ backup(){
         echo -e "${RED}The folder doesn't exits${NC}"
         return
     fi
-
+    
     if [[ ! -d $backup_dir ]]; then
-    echo -e "${BLUE}Creating a backup directory...${NC}"
-    mkdir $backup_dir
-    echo "Done"
+        echo -e "${BLUE}Creating a backup directory...${NC}"
+        mkdir $backup_dir
+        echo "Done"
     fi
-
+    
     date=$(date +"%s")
     desination="$backup_dir/$(basename $path)_$date"
-
+    
     echo -e "${BOLD_BLUE}Backing up your folder...${NC}"
     cp -r $path $desination
     echo "Done"
-    echo -e "Backed up ${BOLD_YELLOW}$path ${NC}to ${BOLD_YELLOW}$desination${NC}" 
-    return  
+    echo -e "Backed up ${BOLD_YELLOW}$path ${NC}to ${BOLD_YELLOW}$desination${NC}"
+    return
 }
 
 
