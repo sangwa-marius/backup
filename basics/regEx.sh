febc4bc38b00b0073b541ce8e6315ea549c1e1eb
Marius <mariussangwa@gmail.com>
2026-06-02 20:52:15 +0200

    formatted the regEx script codes

--- basics/regEx.sh
+++ basics/regEx.sh
@@ -10,17 +10,17 @@ num='[0-9]'
 special='[^a-zA-Z0-9]'
 
 while [[ ! $password =~ $lower || ! $password =~ $upper || ! $password =~ $num || ! $password =~ $special ]]; do
-if [[ ! $password =~ $lower ]]; then
-echo -e "${RED}Password should contain atleast one lowercase character${NC}\n"
-elif [[ $passwd =~ $upper ]]; then
-echo -e "${RED}Password should contain atleast one uppercase character${NC}\n"
-elif [[ ! $password =~ $num ]]; then
-echo -e "${RED}Password should contain atleast one number${NC}\n"
-elif [[ ! $password =~ $special ]]; then 
-echo -e "${RED}Password should contain atleast one special character${NC}\n"
-fi
-
-read -p "Re-Enter the password: " password
+    if [[ ! $password =~ $lower ]]; then
+        echo -e "${RED}Password should contain atleast one lowercase character${NC}\n"
+        elif [[ $passwd =~ $upper ]]; then
+        echo -e "${RED}Password should contain atleast one uppercase character${NC}\n"
+        elif [[ ! $password =~ $num ]]; then
+        echo -e "${RED}Password should contain atleast one number${NC}\n"
+        elif [[ ! $password =~ $special ]]; then
+        echo -e "${RED}Password should contain atleast one special character${NC}\n"
+    fi
+    
+    read -p "Re-Enter the password: " password
 done
 
 echo -e "${BOLD_BLUE}Welcom!!!!${NC}\n"
