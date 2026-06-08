09aecd70df56a99c2bc5b7cf4971e1dfe05d6da9
Marius <mariussangwa@gmail.com>
2026-06-08 22:44:52 +0200

    formatted the sed script

--- basics/Fileoperations/sed.sh
+++ basics/Fileoperations/sed.sh
@@ -8,10 +8,10 @@ data=("filePath" "old_text" "new_text")
 declare -A prompt_data
 
 for datum in "${data[@]}"; do
-read  -p "Enter ${datum}: " value
-prompt_data["${datum}"]=$value
+    read  -p "Enter ${datum}: " value
+    prompt_data["${datum}"]=$value
 done
 
-sed -i "s/${prompt_data[old_text]}/${prompt_data[new_text]}/g" "${prompt_data[filePath]}" 
+sed -i "s/${prompt_data[old_text]}/${prompt_data[new_text]}/g" "${prompt_data[filePath]}"
 count=$(grep -c "${prompt_data[old_text]}" "${prompt_data[filePath]}")
 echo -e "\n${YELLOW}The total replacements were ${BOLD_YELLOW}$count${NC}"
