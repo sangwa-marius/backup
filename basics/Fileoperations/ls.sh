16cc4038a6fedc8bc653299b20dde63e67712ed5
Marius <mariussangwa@gmail.com>
2026-06-10 16:13:20 +0200

    added validations for missing files or directories

--- basics/Fileoperations/ls.sh
+++ basics/Fileoperations/ls.sh
@@ -2,18 +2,27 @@
 source ~/.colors.sh
 count=0
 
-# if [[ ! -d "$1" || ! -f "$1" ]]; then
-# echo "table:cannot access $1: No such file or directory"
-# exit
-# fi
-
+if [[ ! -d $1 && ! -f $1 ]]; then
+echo -e "${RED}[Error]: No such file of directory, $1 ${NC}"
+exit
+fi
 ls -lhsi  $1 > ~/detail.txt
+
+file_color=""
 
 while FS= read -r inode du permissions links owner group size month day Time file_name; do
 ((count++))
 if [[ $count -eq 1 ]]; then
 continue
 fi
-printf "${GREEN}%-10s %-10s %-10s %-10s %-10s %-10s %-10s %-3s %-3s %-10s ${BOLD_BLUE}%s\n" "$inode" "$du" "$permissions" "$links" "$owner" "$group" "$size" "$month" "$day" "$Time" "$file_name"
-echo -e "${GREEN}───────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────${NC}"
+
+if [[ -d "$1/$file_name" ]]; then
+file_color="${BOLD_BLUE}"
+elif [[ -f "$1/$file_name" && -x $file_name ]]; then
+file_color="${BOLD_GREEN}"
+elif [[ -f "$1/$file_name" && ! -x $file_name ]]; then
+file_color="${GREEN}"
+fi
+printf "%-10s %-10s %-10s %-10s %-10s %-10s %-10s %-3s %-3s %-10s $file_color%s${NC}\n" "$inode" "$du" "$permissions" "$links" "$owner" "$group" "$size" "$month" "$day" "$Time" "$file_name"
+echo -e "───────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────"
 done < ~/detail.txt
