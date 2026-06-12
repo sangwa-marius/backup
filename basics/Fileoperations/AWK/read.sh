2dd09f8d82cd2d35818439a3e8d377089d7f788e
Marius <mariussangwa@gmail.com>
2026-06-12 22:00:35 +0200

    formatted the read.sh script using bash beauty

--- basics/Fileoperations/AWK/read.sh
+++ basics/Fileoperations/AWK/read.sh
@@ -9,9 +9,9 @@ count=0
 sum=0
 
 while IFS=, read -r names email marks; do
-
+    
     ((count++))
-
+    
     sum=$((sum+marks))
     if [[ $count -eq 1 ]]; then
         continue
@@ -23,18 +23,18 @@ while IFS=, read -r names email marks; do
     if [[ $marks -lt 50 ]]; then
         marks_color="${BOLD_RED}"
         grade="F"
-
+        
         elif [[ $marks -ge 50 && $marks -lt 70 ]]; then
         marks_color="${BOLD_PURPLE}"
         grade="C"
-
+        
         elif [[ $marks -ge 70 && $marks -lt 90 ]]; then
         marks_color="${BOLD_YELLOW}"
         grade="B"
     else
         marks_color="${BOLD_GREEN}"
         grade="A"
-
+        
     fi
     
     printf "+------+----------------------------+------------------------------+----------+-----------+\n"
@@ -47,19 +47,19 @@ average="Average Marks"
 if [[ $average_marks -lt 50 ]]; then
     marks_color="${BOLD_RED}"
     grade="F"
-
+    
     elif [[ $average_marks -ge 50 && $marks -lt 70 ]]; then
     marks_color="${BOLD_PURPLE}"
     grade="C"
-
+    
     elif [[ $average_marks -ge 70 && $marks -lt 90 ]]; then
     marks_color="${BOLD_YELLOW}"
     grade="B"
-
+    
 else
     marks_color="${BOLD_GREEN}"
     grade="A"
-
+    
 fi
 printf "+------+----------------------------+------------------------------+----------+-----------+\n"
 printf "|${BOLD_GREEN}%-83s $marks_color%s${NC}   |\n"  "  $average"   "$average_marks"
