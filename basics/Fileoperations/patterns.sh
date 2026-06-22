9f4b7c7503947f05d13a8f65f68bd4780dd0bee1
Marius <mariussangwa@gmail.com>
2026-06-22 12:01:18 +0200

    changed the sym_pattern

--- basics/Fileoperations/patterns.sh
+++ basics/Fileoperations/patterns.sh
@@ -2,4 +2,4 @@
 
 email_pattern="[a-zA-Z0-9.+%_]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}"
 file_pattern="^[a-zA-Z0-9.]+\.([a-zA-Z0-9]+$)"
-symlink_pattern='[a-zA-Z0-9]+ -> [a-zA-Z0-9./-_]+$'
+symlink_pattern='.+ -> .+$'
