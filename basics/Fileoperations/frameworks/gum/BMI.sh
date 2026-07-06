53ab82d6a4885cb7b42798f360202e80df39d455
Marius <mariussangwa@gmail.com>
2026-07-06 19:08:28 +0200

    implemented DRY in BMI script

--- basics/Fileoperations/frameworks/gum/BMI.sh
+++ basics/Fileoperations/frameworks/gum/BMI.sh
@@ -5,32 +5,33 @@ source ~/.colors.sh
 
 clear
 gum style \
---foreground 55 \
+--foreground 50 \
 --border double \
---border-foreground 55 \
+--border-foreground 40 \
 --bold \
 --width 50 \
 --align center \
 "CHECK YOUR BODY MASS INDEX"
 
-weight=$(gum input --placeholder="Enter your mass (Kg)")
-height=$(gum input --placeholder="Enter your height (metres)")
-square_height=$(echo "scale=5; $height*$height" | bc )
-BMI=$(echo " scale=5; $weight/$square_height" | bc )
-
-
-
-printf "Your Body Mass Index is ${BOLD_CYAN}%.1f${BOLD_GREEN}\n" $BMI
-
-choice=$(gum confirm "Wanna check for more?" && echo "yes" || echo "no")
-
-while [[ $choice == "yes" ]]; do
-    weight=$(gum input --placeholder="Enter your mass (Kg)")
-    height=$(gum input --placeholder="Enter your height (metres)")
+while true; do
+    weight=$(
+        gum input \
+        --placeholder="Enter your mass (Kg)" \
+        --placeholder.foreground 35 \
+        --prompt.foreground 35 \
+        --cursor.foreground 35
+    )
+    
+    height=$(
+        gum input \
+    --placeholder="Enter your height (metres)" --placeholder.foreground 35 --prompt.foreground 35 --cursor.foreground 35)
     square_height=$(echo "scale=5; $height*$height" | bc )
     BMI=$(echo " scale=5; $weight/$square_height" | bc )
     printf "Your Body Mass Index is ${BOLD_CYAN}%.1f${BOLD_GREEN}\n" $BMI
-    choice=$(gum confirm "Wanna check for more?" && echo "yes" || echo "no")
+    if ! gum confirm "Wanna check for more?" --prompt.foreground 40 --selected.background 40 --unselected.foreground 60; then
+        break
+    fi
+    
 done
 
 echo -e "${BOLD_YELLOW}Thank you. Hope you will be back soon${NC}"
