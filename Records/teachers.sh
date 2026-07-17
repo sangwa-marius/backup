c84c8b3b6c0475ae64e0dcfc63da881d080f1ad9
Marius <mariussangwa@gmail.com>
2026-07-17 23:33:59 +0200

    formatted the teachers recording script

--- Records/teachers.sh
+++ Records/teachers.sh
@@ -41,57 +41,57 @@ fields=(
 )
 
 while true; do
-
-declare -A teacher_information
-
-for field in "${fields[@]}"; do
     
-    teacher_information[$field]=$(
-        gum input \
-        --placeholder "Enter $field" \
-        --prompt.foreground $ELECTRIC_BLUE \
-        --cursor.foreground $ELECTRIC_BLUE \
-    )
+    declare -A teacher_information
     
-    value="${teacher_information[$field]}"
-    
-    if [[ -z $value ]]; then
-        gum style  --foreground $RED "$field cannot be empty. Exiting..."
-        exit 1
-    fi
-    
-    if [[ "$field" == "Email" && ! "$value" =~ $email_pattern ]]; then
-        gum style  --foreground $RED "Invalid Email formart. Exiting..."
-        sleep 2
-        exit 1
-    fi
-    
-    if [[ "$field" == "Age" ]]; then
-        if [[ "$value" =~ $not_valid_positive_integer ]]; then
-            gum style  --foreground $RED "Age should be a valid positive integer. Exiting..."
+    for field in "${fields[@]}"; do
+        
+        teacher_information[$field]=$(
+            gum input \
+            --placeholder "Enter $field" \
+            --prompt.foreground $ELECTRIC_BLUE \
+            --cursor.foreground $ELECTRIC_BLUE \
+        )
+        
+        value="${teacher_information[$field]}"
+        
+        if [[ -z $value ]]; then
+            gum style  --foreground $RED "$field cannot be empty. Exiting..."
+            exit 1
+        fi
+        
+        if [[ "$field" == "Email" && ! "$value" =~ $email_pattern ]]; then
+            gum style  --foreground $RED "Invalid Email formart. Exiting..."
             sleep 2
             exit 1
         fi
         
-        if [[ $value -le 0 ]]; then
-            gum style  --foreground $RED "$value years really? Exiting..."
-            sleep 2
-            exit 1
+        if [[ "$field" == "Age" ]]; then
+            if [[ "$value" =~ $not_valid_positive_integer ]]; then
+                gum style  --foreground $RED "Age should be a valid positive integer. Exiting..."
+                sleep 2
+                exit 1
+            fi
+            
+            if [[ $value -le 0 ]]; then
+                gum style  --foreground $RED "$value years really? Exiting..."
+                sleep 2
+                exit 1
+            fi
         fi
-    fi
-
-
-done
+        
+        
+    done
 cat <<EOF >> $Db_File
 [$(date +"%Y-%m-%d %H:%M:%S")],${teacher_information["Full name"]},${teacher_information["Email"]},${teacher_information["Age"]},${teacher_information["Subject"]},${teacher_information["School"]}
 EOF
-
-if [[ $? -eq 0 ]]; then
-gum style --foreground $FOREST_GREEN "Information saved to $Db_File"
-fi
-
-if ! gum confirm "Wanna record more teachers?" --prompt.foreground $ELECTRIC_BLUE --selected.background $FOREST_GREENk; then
-gum style --foreground $ELECTRIC_BLUE "Thank you"
-break
-fi
+    
+    if [[ $? -eq 0 ]]; then
+        gum style --foreground $FOREST_GREEN "Information saved to $Db_File"
+    fi
+    
+    if ! gum confirm "Wanna record more teachers?" --prompt.foreground $ELECTRIC_BLUE --selected.background $FOREST_GREENk; then
+        gum style --foreground $ELECTRIC_BLUE "Thank you"
+        break
+    fi
 done
