44da8b1aecd36d2d95ae6b0bfa96a67a3e6a64df
Marius <mariussangwa@gmail.com>
2026-08-19 17:51:55 +0200

    added a script to view students from mysql database

---
+++ View/Mysql/viewStudents.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo ""
+
+gum spin --spinner "dot"  --title "Fetching data" --title.foreground 40 --spinner.foreground 40 -- sleep 3 
+echo ""
+mysql -u "$Db_user" -p"$Db_password" "$Db_name" -B -e "SELECT * FROM students;" | tr "\t" "," | gum table --selected.foreground 99 --header.foreground 40
