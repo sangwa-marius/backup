220a6094d44b3a5cbfbf9c2b81d27d20863e45a2
Marius <mariussangwa@gmail.com>
2026-08-20 21:16:52 +0200

    formatted the view script for mysql

--- View/Mysql/viewStudents.sh
+++ View/Mysql/viewStudents.sh
@@ -3,11 +3,11 @@
 echo ""
 
 gum spin \
- --spinner "dot"  \
- --title "Fetching data" \
- --title.foreground 40 \
- --spinner.foreground 40 \
- -- sleep 3
+--spinner "dot"  \
+--title "Fetching data" \
+--title.foreground 40 \
+--spinner.foreground 40 \
+-- sleep 3
 echo ""
 mysql -u "$Db_user" -p"$Db_password" "$Db_name" -B -e "
 SELECT * FROM students;" | tr "\t" "," | gum table \
