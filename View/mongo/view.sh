964ba0e99b58fbe7ef9dcee99b298b7cf6b0559f
Marius <mariussangwa@gmail.com>
2026-08-21 21:47:33 +0200

    formatted the view script of mongo

--- View/mongo/view.sh
+++ View/mongo/view.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
 if [[ -z $MONGO_URL ]]; then
-echo MONGO_URL not set
-exit
+    echo MONGO_URL not set
+    exit
 fi
 
 raw_json=$(mongosh "$MONGO_URL" --json=canonical --quiet --eval "db.students.find().toArray()")
 
 if [[ -z "$raw_json" || "$raw_json" == null ]]; then
-echo "User not found"
-exit
+    echo "User not found"
+    exit
 fi
 
 colored_json=$(echo "$raw_json" | jq -C '.')
 
 gum style \
-    --border rounded \
-    --border-foreground 40 \
-    --padding "1 2" \
-    "$colored_json"
+--border rounded \
+--border-foreground 40 \
+--padding "1 2" \
+"$colored_json"
