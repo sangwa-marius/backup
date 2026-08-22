470108714124426ec119fdbc49c3bbadb8a6ce23
Marius <mariussangwa@gmail.com>
2026-08-22 22:46:59 +0200

    added loading states to both fetching all student names and feaching details for selected student

--- View/mongo/viewAdvanced.sh
+++ View/mongo/viewAdvanced.sh
@@ -1,11 +1,24 @@
 #!/usr/bin/env bash
 
+clear
+
 if [[ -z "$MONGO_URL" ]]; then
     echo MONGO_URL not set
     exit 1
 fi
 
-raw_json=$(mongosh "$MONGO_URL" --json=relaxed --quiet --eval 'db.students.find().toArray()')
+fetch_all_student_names(){
+    mongosh "$MONGO_URL" --json=relaxed --quiet --eval 'db.students.find().toArray()'
+}
+
+raw_json=$(
+    gum spin \
+    --spinner dot \
+    --title "Loading all student names" \
+    -- bash -c "$(declare -f fetch_all_student_names);fetch_all_student_names"
+)
+
+
 selected_student=$(echo "$raw_json" | jq -r  '"\(.[].name)"' | gum filter)
 
 if [[ -z $selected_student ]]; then
@@ -13,27 +26,26 @@ if [[ -z $selected_student ]]; then
     exit 1
 fi
 
-echo "Detailed information about $selected_student"
-
-selected_student_info
-
 fetch_selected_student_info(){
-selected_student_info=$(mongosh "$MONGO_URL" --quiet --json=relaxed --eval "db.students.findOne({name:'${selected_student}'})" | jq -r '"
+    mongosh "$MONGO_URL" --quiet --json=relaxed --eval "db.students.findOne({name:'$1'})" | jq -r '"
 Student name : \(.name)
 Student email: \(.email)
 School       : \(.school)
 age          : \(.age)
-"')
+    "'
 }
 
-gum spin \
---spinner dot \
--- bash -c 'fetch_selected_student_info'
-
+selected_student_info=$(
+    gum spin \
+    --spinner dot \
+    --title "Loading details for $selected_student" \
+    -- bash -c "$(declare -f fetch_selected_student_info); fetch_selected_student_info \"$selected_student\""
+)
 
 gum style \
 --border normal \
 --border-foreground 40 \
---padding "1 2" \
+--padding "0 2" \
+--margin "1 0" \
 "$selected_student_info"
 
